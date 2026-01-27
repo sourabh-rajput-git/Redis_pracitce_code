59302309743cfe2362d8cd860823a8d71102e04d
sourabh-rajput-git <sourabhcandidate@gmail.com>
2026-01-27 17:23:39 +0530

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,14 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from database import Base, get_db
 from main import app
 
 # Use SQLite for tests
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
+Base = declarative_base()
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False}
@@ -26,9 +26,6 @@ def override_get_db():
         yield db
     finally:
         db.close()
-
-# Override dependency
-app.dependency_overrides[get_db] = override_get_db
 
 @pytest.fixture(scope="session")
 def db():
