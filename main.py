878e2341c3fcea088ee0a73f5bd4dcff457b0c93
sourabh-rajput-git <sourabhcandidate@gmail.com>
2026-01-27 17:33:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
-from database import Base, engine
 import users as u
-
-#this creates all tables with all associated with Base.metadata into the database
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
