761f4da447d706b9925062ac8210e96cbf668074
LAP #0041 <sourabh.candidate@gmail.com>
2026-01-28 15:12:09 +0530

    Updated database.py to fix Neon timeout issue

--- database.py
+++ database.py
@@ -7,7 +7,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DB_URL")
 
-engine = create_engine(DATABASE_URL)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,   # checks connection before using it
+    pool_recycle=300,     # avoids Neon idle timeout
+)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
